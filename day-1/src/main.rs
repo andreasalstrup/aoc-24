9be27ec47ffa1623f7459d0d8c85239dfc354fae
Andreas Knudsen Alstrup <aalstr20@student.aau.dk>
2024-12-02 13:17:42 +0100

    Solve day-1 part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Result;
@@ -16,7 +17,7 @@ fn parse_input(buffer: &str) -> (Vec<&str>, Vec<&str>) {
     (list_a, list_b)
 }
 
-fn total_distance(sorted_list_a: Vec<&str>, sorted_list_b: Vec<&str>) -> u32 {
+fn total_distance(sorted_list_a: &Vec<&str>, sorted_list_b: &Vec<&str>) -> u32 {
     let mut result = Vec::new();
 
     for i in 0..sorted_list_a.len() {
@@ -29,6 +30,44 @@ fn total_distance(sorted_list_a: Vec<&str>, sorted_list_b: Vec<&str>) -> u32 {
     result.iter().sum()
 }
 
+fn similarity_score(list_a: &Vec<&str>, list_b: &Vec<&str>) -> u32 {
+    let mut result = 0;
+    let mut similarity_map: HashMap<&str, u32> = HashMap::new();
+    let mut iter_a = list_a.into_iter();
+    let mut iter_b = list_b.into_iter().peekable();
+
+    // Fast AF
+    while let Some(a) = iter_a.next() {
+        while let Some(&b) = iter_b.peek() {
+            if a > b {
+                iter_b.next();
+            } else if a == b {
+                similarity_map.entry(a).and_modify(|v| *v += 1).or_insert(1);
+                iter_b.next();
+            } else {
+                break;
+            }
+        }
+    }
+
+    // Slow AF
+    // for a in list_a {
+    //     for b in list_b {
+    //         if a == b {
+    //             similarity_map.entry(a).and_modify(|v| *v += 1).or_insert(1);
+    //         } else {
+    //             similarity_map.entry(a).or_insert(0);
+    //         }
+    //     }
+    // }
+
+    for key in similarity_map.keys() {
+        result += key.parse::<u32>().unwrap() * similarity_map[key];
+    }
+
+    result
+}
+
 fn main() -> Result<()> {
     let mut file = File::open("./day-1/input-1.txt")?;
     let mut buffer = String::new();
@@ -39,7 +78,8 @@ fn main() -> Result<()> {
     list_a.sort();
     list_b.sort();
 
-    println!("Total Distance: {}", total_distance(list_a, list_b));
+    println!("Total Distance: {}", total_distance(&list_a, &list_b));
+    println!("Similarity_score: {}", similarity_score(&list_a, &list_b));
 
     Ok(())
 }
